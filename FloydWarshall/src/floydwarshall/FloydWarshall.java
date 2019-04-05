3dd665465779cdb97477f362992c79cd1b641b76
Jonathan Caballero Castillo <jonathancaballero@CAMPUSUNINORTE.LOCAL>
2019-04-05 10:37:42 -0500

    crear método FW

--- FloydWarshall/src/floydwarshall/FloydWarshall.java
+++ FloydWarshall/src/floydwarshall/FloydWarshall.java
@@ -1,12 +1,46 @@
-
 package floydwarshall;
-
 
 public class FloydWarshall {
 
-    
-    public static void main(String[] args) {
-        
+    static void floyd_warshall(int grafo[][]) {
+        int n = grafo.length, tmp;
+        //crear matriz de distancia
+        int caminos[][] = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                caminos[i][j] = j;
+            }
+        }
+
+        //ejecutar el método floy-marshall
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < n; k++) {
+                    tmp = grafo[i][k] + grafo[k][j];
+                    if (tmp < grafo[i][j]) {
+                        grafo[i][j] = tmp;
+                        caminos[i][j] = k;
+                    }
+                }
+            }
+        }
+        System.out.println("La matriz de distancias ");
+        print(grafo);
+        System.out.println("La matriz de caminos");
+        print(caminos);
     }
     
+    static void print (int M[][]){
+        System.out.println("");
+        for (int i = 0; i < M.length; i++) {
+            for (int j = 0; j < M.length; j++) {
+                System.out.println(" "+M[i][j]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
 }
